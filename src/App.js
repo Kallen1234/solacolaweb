2d264d6f2a5a16eab63912fc136b87b14e4bdd80
kallen <kallenschulz@gmail.com>
2024-04-02 09:39:18 -0500

    added CA and made the site responsive

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { CSSTransition } from "react-transition-group";
 function App() {
   const [showLogo, setShowLogo] = useState(false);
   const [showButtons, setShowButtons] = useState(false);
+  const cryptoAddress = "CGQwZ8U3rLU1iWVZjm4hsJTSLbkT2eLKC5LWfaZuwRVP";
 
   setTimeout(() => {
     setShowLogo(true);
@@ -14,6 +15,13 @@ function App() {
     setShowButtons(true);
   }, 1500);
 
+  const handleCryptoAddressClick = () => {
+    const raydiumSwapURL = `https://raydium.io/swap/?inputCurrency=sol&outputCurrency=${encodeURIComponent(
+      cryptoAddress
+    )}&fixed=in`;
+    window.open(raydiumSwapURL, "_blank");
+  };
+
   return (
     <div className="container">
       <CSSTransition
@@ -22,8 +30,18 @@ function App() {
         classNames="logo"
         unmountOnExit
       >
-        <img src={require("./solaCola1.png")} alt="Logo" />
+        <div className="logo-wrapper">
+          <img
+            src={require("./solaCola1.png")}
+            alt="Logo"
+            className="logo-img"
+          />
+        </div>
       </CSSTransition>
+      <div className="crypto-address" onClick={handleCryptoAddressClick}>
+        <span className="crypto-address-label">Crypto Address:</span>
+        <p>CGQwZ8U3rLU1iWVZjm4hsJTSLbkT2eLKC5LWfaZuwRVP</p>
+      </div>
       <CSSTransition
         in={showButtons}
         timeout={1000}
@@ -35,22 +53,31 @@ function App() {
             href="https://twitter.com/solacolasolana"
             target="_blank"
             rel="noopener noreferrer"
+            className="link-button"
           >
-            <button>Twitter</button>
+            <button>
+              <i className="fab fa-twitter"></i> Twitter
+            </button>
           </a>
           <a
             href="https://t.me/solacolacto"
             target="_blank"
             rel="noopener noreferrer"
+            className="link-button"
           >
-            <button>Telegram</button>
+            <button>
+              <i className="fab fa-telegram"></i> Telegram
+            </button>
           </a>
           <a
             href="https://dexscreener.com/solana/73xdr7mzayhsfga6qqig5u2m2u2htnwxtbyj2xawm4mt"
             target="_blank"
             rel="noopener noreferrer"
+            className="link-button"
           >
-            <button>Dexscreener</button>
+            <button>
+              <i className="fas fa-chart-line"></i> Dexscreener
+            </button>
           </a>
         </div>
       </CSSTransition>
