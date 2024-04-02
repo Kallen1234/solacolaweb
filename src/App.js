045106d1a5a6837dfb1d62de6e0af826a16c7b90
kallen <kallenschulz@gmail.com>
2024-04-01 22:37:32 -0500

    Initial commit and website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
+  const [showLogo, setShowLogo] = useState(false);
+  const [showButtons, setShowButtons] = useState(false);
+
+  setTimeout(() => {
+    setShowLogo(true);
+  }, 500);
+
+  setTimeout(() => {
+    setShowButtons(true);
+  }, 1500);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <CSSTransition
+        in={showLogo}
+        timeout={1000}
+        classNames="logo"
+        unmountOnExit
+      >
+        <img src={require("./solaCola.jpg")} alt="Logo" />
+      </CSSTransition>
+      <CSSTransition
+        in={showButtons}
+        timeout={1000}
+        classNames="buttons"
+        unmountOnExit
+      >
+        <div className="button-container">
+          <a
+            href="https://twitter.com/solacolasolana"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Twitter</button>
+          </a>
+          <a
+            href="https://t.me/solacolacto"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Telegram</button>
+          </a>
+          <a
+            href="https://dexscreener.com/solana/73xdr7mzayhsfga6qqig5u2m2u2htnwxtbyj2xawm4mt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Dexscreener</button>
+          </a>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
